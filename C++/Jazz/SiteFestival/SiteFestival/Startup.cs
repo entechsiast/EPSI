047790bb1755e632a65ed9e8e6a757ded7de9032
zatharan <jg_972@hotmail.com>
2017-05-24 11:24:26 +0200

    Dev C++

---
+++ C++/Jazz/SiteFestival/SiteFestival/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(SiteFestival.Startup))]
+namespace SiteFestival
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
