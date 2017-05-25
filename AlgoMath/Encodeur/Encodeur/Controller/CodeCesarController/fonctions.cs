74a5ff5cbcc1b40aa98e03e4b4c2f6aafcec754c
zatharan <jg_972@hotmail.com>
2017-05-25 15:56:05 +0200

    commentaire

--- AlgoMath/Encodeur/Encodeur/Controller/CodeCesarController/fonctions.cs
+++ AlgoMath/Encodeur/Encodeur/Controller/CodeCesarController/fonctions.cs
@@ -27,13 +27,16 @@ namespace Encodeur.Controller.CodeCesarController
                 //le caractère chiffré prend la valeur de l'index deu caractère à crypter + la valeur de la clé de cryptage
                 caractereChiffre = (char)(unCaractere + cle);
 
-
-                // la simple cote indique qu'il s'agit d'un caractère et non d'une string
-                // si la valeur de l'index du caractère est supérieure à la valeur de z ou Z (différents caractères dans la table ASCII)
+                //Cette partie permet de recommencer l'opération depuis la lettre a quand z est dépassé
+                    // la simple cote indique qu'il s'agit d'un caractère et non d'une string
+                    // si la valeur de l'index du caractère est supérieure à la valeur de z ou Z (différents caractères dans la table ASCII)
                 if (caractereChiffre > 'z')
                 {
+                    //fait la différence entre la valeur de z et la valeur du caractère chiffré
                     char differenceChiffreEtZ = (char)(caractereChiffre - 'z');
+                    //soustrait la différence précédente à la valeur de la clé de cryptage
                     char differenceCleResteChiffreEtZ = (char)(cle - differenceChiffreEtZ);
+                    //ajoute le résultat de l'opéation précédente moins 1 pour obtenir le bon résultat
                     caractereChiffre =(char) (('a' + differenceChiffreEtZ)-1);
                 }
                 //contruit la chaine de caractère cryptée en y ajoutant à chaque boucle le chiffre résultant de l'opération
