5ce400e7cbdddea059a731c5a209899ee056be6a
jonathan <jg_972@hotmail.com>
2017-06-04 21:55:32 +0200

    commit PGCD et Decimal To Binaire

--- AlgoMath/DecimalToBinaire/DecimalToBinaire/Program.cs
+++ AlgoMath/DecimalToBinaire/DecimalToBinaire/Program.cs
@@ -10,6 +10,38 @@ namespace DecimalToBinaire
     {
         static void Main(string[] args)
         {
+            //variable declaration
+            int dividende, quotient, reste;
+            int diviseur = 2;
+            List<int> stockreste = new List<int>();
+
+
+            ////UI
+            Console.WriteLine("Saisissez un nombre decimal puis appuyez sur la toucher ENTREE");
+            dividende = int.Parse(Console.ReadLine()); // converti la string en int pour la stocker dans dividende
+
+            while (dividende / 2 >= 1)
+            {
+                quotient = dividende / 2;
+                Console.WriteLine("le quotient est de " + quotient);
+                reste = dividende % 2;
+                stockreste.Add(reste); // ajoute à la liste la valeur de reste
+                Console.WriteLine("le reste est de " + reste);
+                dividende = quotient;
+                Console.WriteLine("la valeur du dividende est maintenant de " + dividende);
+                if (quotient == 1) // dans le cas d'un nombre impaire ajouter la valeur du quotient en reste pour que le nombre binaire soit valide.
+                {
+                    stockreste.Add(quotient);
+                }
+            }
+            
+            foreach(int element in stockreste)
+            {
+                Console.Write(element.ToString());
+            }
+
+            Console.ReadLine();
+
         }
     }
 }
---
+++ AlgoMath/PGCD/PGCD/Program.cs
@@ -0,0 +1,55 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PGCD
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int nb1, nb2, nb3 ;
+            int plusGrandNombre, plusPetitNombre, quotient;
+            int reste, Dividende,Diviseur ; // initialisé à 1 pour entrer dans la boucle
+
+            #region UI
+            Console.WriteLine("saisissez le 1er nombre");
+            nb1 = int.Parse(Console.ReadLine()); // recupère le premier nombre
+
+            Console.WriteLine("saisissez le 2eme nombre");
+            nb2 = int.Parse(Console.ReadLine()); // recupère le 2eme nombre
+            #endregion
+
+            #region Défini les plus grand et plus petits nombres du couple nb1 nb2
+            Dividende = Math.Max(nb1, nb2); // selectionne le plus grand nombre du couple nb1 nb2, qui sera le dividende
+            Diviseur = Math.Min(nb1, nb2); // selectionne le plus petit nombre du couple nb1 nb2, qui sera le diviseur
+            #endregion
+
+            #region fait la division du plus grand nombre par le plus petit nombre et récupère le reste
+            // le reste deviens le nouveau diviseur, le reste de la division étant toutjours inférieur au diviseur ici;
+            reste = Dividende % Diviseur;
+            #endregion
+
+            Dividende = Diviseur;
+
+            while (reste != 0)
+            {
+                #region le reste se transforme en diviseur de la nouvelle division et le diviseur de l'ancienne se transforme en dividende de la nouvelle
+                Diviseur = reste;
+                #endregion
+
+                #region calcul le reste de la nouvelle Division
+                reste = Dividende % Diviseur;
+                #endregion
+                Dividende = Math.Max(reste, Diviseur);
+            }
+
+            Console.Write("le PGC de " + nb1 + " et " + nb2 + " est " + Diviseur);
+
+
+            Console.ReadLine();
+        }
+    }
+}
