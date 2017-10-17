a95e79e5de73336236a11deab78ac3582ef67ee5
En_Tech_Siast <jg_972@hotmail.com>
2017-10-17 11:46:08 +0200

    clean init

---
+++ IP/ProjetBluetooth/projetBluetooth/projetBluetooth/Acceuil.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace projetBluetooth
+{
+    public partial class Acceuil : Form
+    {
+        public Acceuil()
+        {
+            InitializeComponent();
+        }
+    }
+}
